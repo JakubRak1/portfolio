70428be7aaee3710cb645dd8bfc00c9bb7034d69
Jakub Rak <kubarak41@gmail.com>
2022-06-18 18:39:43 +0200

    Reworked nav bar with hamburger add some transmition to nav bar and a bit code cleaning

--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
+/* Nav bar */
+let hamburgerTransmition = document.getElementById(`hambNav`);
+let hamb1 = document.getElementById(`firstHamb`);
+let hamb2 = document.getElementById(`secondHamb`);
+let hamb3 = document.getElementById(`thirdHamb`);
+let navList = document.getElementById(`navList`);
+
+
+
+/* Popup Windows */
 let project1Btn = document.getElementById(`project1`);
 let project2Btn = document.getElementById(`project2`);
 let closeProject1Btn = document.getElementById(`closeProject1`);
 let closeProject2Btn = document.getElementById(`closeProject2`);
 let project_1 = document.getElementById(`windowProject1`);
 let project_2 = document.getElementById(`windowProject2`);
+
+hamburgerTransmition.onclick = function (){
+    hamb1.classList.toggle(`change1`);
+    hamb2.classList.toggle(`change2`);
+    hamb3.classList.toggle(`change3`);
+    hamburgerTransmition.classList.toggle(`change4`);
+    navList.classList.toggle(`visibility`);
+};
+
+
+
+
+
 
 project1Btn.onclick = function (){
   project_1.style.display="block";
@@ -18,3 +41,4 @@ project2Btn.onclick = function (){
 closeProject2Btn.onclick = function (){
     project_2.style.display="none";
 };
+
