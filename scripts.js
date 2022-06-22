10e9655229e7c4b5a6c4ccd2928ad52eb6763db8
Jakub Rak <kubarak41@gmail.com>
2022-06-22 22:36:25 +0200

    Fixed small bugs in styles and changed content to real

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 /* Nav bar */
 let navBar = document.getElementById(`navBar`);
-let headText = document.getElementById(`head`);
 let hamburgerTransmition = document.getElementById(`hambNav`);
 let hamb1 = document.getElementById(`firstHamb`);
 let hamb2 = document.getElementById(`secondHamb`);
@@ -12,10 +11,16 @@ let navList = document.getElementById(`navList`);
 /* Popup Windows */
 let project1Btn = document.getElementById(`project1`);
 let project2Btn = document.getElementById(`project2`);
+let project3Btn = document.getElementById(`project3`);
+let project4Btn = document.getElementById(`project4`);
 let closeProject1Btn = document.getElementById(`closeProject1`);
 let closeProject2Btn = document.getElementById(`closeProject2`);
+let closeProject3Btn = document.getElementById(`closeProject3`);
+let closeProject4Btn = document.getElementById(`closeProject4`);
 let project_1 = document.getElementById(`windowProject1`);
 let project_2 = document.getElementById(`windowProject2`);
+let project_3 = document.getElementById(`windowProject3`);
+let project_4 = document.getElementById(`windowProject4`);
 
 /* Burger bar */
 hamburgerTransmition.onclick = function (){
@@ -25,14 +30,10 @@ hamburgerTransmition.onclick = function (){
     hamburgerTransmition.classList.toggle(`change4`);
     navList.classList.toggle(`visibility`);
     navBar.classList.toggle(`navBarFixed`);
-    headText.classList.toggle(`headCenter`);
 };
 
 
-
-
-
-
+/* Pop Windows */
 project1Btn.onclick = function (){
   project_1.style.display="block";
 };
@@ -46,4 +47,15 @@ project2Btn.onclick = function (){
 closeProject2Btn.onclick = function (){
     project_2.style.display="none";
 };
-
+project3Btn.onclick = function (){
+    project_3.style.display="block";
+};
+closeProject3Btn.onclick = function (){
+    project_3.style.display="none";
+};
+project4Btn.onclick = function (){
+    project_4.style.display="block";
+};
+closeProject4Btn.onclick = function (){
+    project_4.style.display="none";
+};
